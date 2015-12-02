8f0395b49ee5dd607616d5b0c14e9e211424571a
Navin Mahabir <navinmahabir@Navins-MacBook-Pro.local>
2015-12-01 23:13:24 -0500

    First commit dude

---
+++ app/src/main/java/com/example/navinmahabir/myfirstapp/MyActivity.java
@@ -0,0 +1,81 @@
+package com.example.navinmahabir.myfirstapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class MyActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    public static boolean title;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        title=true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if(id== R.id.action_title){
+            if(title){
+                this.setTitle(R.string.hello_world);
+                title=false;
+            }
+            else{
+                this.setTitle(R.string.app_name);
+                title=true;
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        if (message.equals("")){
+            message="Empty Text";
+        }
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void changeTheme(){
+
+    }
+    public void differentMessage(View view){
+       // Intent intent = new Intent(this)
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String message = "Surprise!!!";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void mylists(View view){
+        Intent intent = new Intent(this, SimpleListActivity.class);
+        startActivity(intent);
+    }
+}
